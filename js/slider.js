2012198bb8a734f9a7d517529eafe1d0f086fb8d
onikesum <122078008+onikesum@users.noreply.github.com>
2023-07-10 16:52:11 +0900

    addDynamic
    동적부분을 수정하고있음..사진은 나타나게 됨

--- js/slider.js
+++ js/slider.js
@@ -9,15 +9,15 @@
 //       prevEl: '.swiper-button-prev',
 //     },
 //   });
-  $(document).ready(function() {
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      centeredSlides: true,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+$(document).ready(function() {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
+});
